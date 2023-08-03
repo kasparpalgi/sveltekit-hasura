f70c55caab8ea34c0cc72a75d7bd850313964ff0
Kaspar L. Palgi <git@e-stonia.co.uk>
2023-08-03 19:37:43 +0300

    Don't let to the /account if not signed in

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,13 +1,28 @@
 <script>
+	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 	import { nhostClient } from '$lib/backend/api';
-	import { isSignedIn, user } from '$lib/backend/api';
+	import { user } from '$lib/backend/api';
+
+	let userInfo = '';
+
+	$: {
+		user.subscribe((value) => {
+			userInfo = value;
+		});
+	}
+
+	onMount(() => {
+		if (!userInfo) {
+			goto('/');
+		}
+	});
 
 	async function logOut() {
-		nhostClient.auth.signOut();
+		await nhostClient.auth.signOut();
 		goto('/');
 	}
 </script>
 
-<h1>My account</h1>
+<h1>Heya {userInfo.displayName}!</h1>
 <button on:click={logOut}>Logout</button>
